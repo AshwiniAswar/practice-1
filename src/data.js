6962c3f839b9e2772c3befa9953341d68066a099
AshwiniAswar <141212068+AshwiniAswar@users.noreply.github.com>
2023-08-05 10:42:11 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,34 @@
+// import React from 'react'
+// import img1 from './https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=wood-wristwatch-time-190819.jpg&fm=jpg';
+import   './image/purse.jpg';
+import './image/watch.jpg';
+const data= {
+  productData:[
+    {
+        id:1,
+        // img:'https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=wood-wristwatch-time-190819.jpg&fm=jpg',
+        title:'grapes',
+        desc:'',
+        price:46,
+
+    },
+    {
+        id:1,
+        img:'purse',
+        title:'mango',
+        desc:'',
+        price:86,
+
+    },
+    {
+        id:1,
+        img:'watch',
+        title:'banana',
+        desc:'',
+        price:60,
+
+    }
+  ],
+}
+
+export default data;
